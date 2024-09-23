d9d263aebe9661cb7e39fbe8747a6faef6e132e3
GawarWawar <gawarwawar@gmail.com>
2024-09-23 21:35:21 +0300

    lay foundation for the DotAMatch class

--- selenium_tryout.py
+++ selenium_tryout.py
@@ -1,42 +1,80 @@
-import selenium
 from selenium import webdriver
-import selenium.webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import NoSuchElementException
 
 
 import time
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--enable-javascript")
+def set_up_driver(
+    driver_options: webdriver.IeOptions|None = None
+):
 
-driver = webdriver.Chrome(options=chrome_options)
+    if driver_options is None:
+        driver_options = Options()
+        # driver_options.add_argument("--headless")
+        driver_options.add_argument("--enable-javascript")
+    return webdriver.Chrome(options=driver_options)
 
-MATCH_ID = 7952112098
+class DotAMatch:
+    def __init__(
+        self, 
+        MATCH_ID, 
+        driver_options: Options = None
+    ) -> None:
+        self.MATCH_ID = MATCH_ID
+        self.driver = set_up_driver(driver_options)
+        self._is_processed = False
+    
+    def check_parsed_status(self) -> bool:
+        self._is_parsed = self._is_parsed_()
+        return self._is_parsed
+        
+    def _is_parsed_(self) -> bool:
+        self.driver.get(f"https://www.opendota.com/matches/{self.MATCH_ID}")
+        self._is_processed = True
+        assert "OpenDota" in self.driver.title
 
-driver.get(f"https://www.opendota.com/matches/{MATCH_ID}")
-assert "OpenDota" in driver.title
+        text_indicator = "The replay for this match has not yet been parsed. Not all data may be available."
+        time.sleep(5)
+        body = self.driver.find_element(By.TAG_NAME, "body")
 
-target_text = "The replay for this match has not yet been parsed. Not all data may be available."
-time.sleep(5)
-    # Try to find the element containing the text
-body = driver.find_element(By.TAG_NAME, "body")
+        if text_indicator in body.text:
+            return False
+        else:
+            return True
+        
+    def parce_match(self) -> str:
+        if self._is_processed:
+            if self._is_parsed: return "Match is already parsed"
+            body = self.driver.find_element(By.TAG_NAME, "body")
+            try:
+                self.driver.find_element(By.CLASS_NAME, "matchButtons")
+            except NoSuchElementException:
+                return "Match could not be parsed"
+            
+            self.driver.get(f"https://www.opendota.com/request#{MATCH_ID}")
+            text_indicator = "Requesxt a Parse"
+            time.sleep(2)
+            while True:
+                    body = self.driver.find_element(By.TAG_NAME, "body")
+                    if text_indicator not in body.text:
+                        break
+            
+            return "Match has been parsed sucsessfully"
 
-if target_text in body.text:
-    match_buttons = body.find_element(By.CLASS_NAME, "matchButtons")
-    driver.get(f"https://www.opendota.com/request#{MATCH_ID}")
-    target_text = "Requesxt a Parse"
-    while True:
-        body = driver.find_element(By.TAG_NAME, "body")
-        if target_text not in body.text:
-            break
-    print("DONE!")
-else:
-    print("Text not found!")
+    
 
-driver.close()
+MATCH_ID = 7948218329
+d_match = DotAMatch(MATCH_ID)
+d_match.check_parsed_status()
+print(d_match.parce_match())
+print(d_match.check_parsed_status())
+
+
+
+
+
+
