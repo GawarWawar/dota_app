23ed368188fae211ed805cfe048aa489370835c7
GawarWawar <gawarwawar@gmail.com>
2024-09-24 19:14:11 +0300

    added calss to scan a profile and added a method to get last match by its id

---
+++ Profile.py
@@ -0,0 +1,49 @@
+from DotaMatch import DotaMatch
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import NoSuchElementException
+
+import time
+import utils
+
+# PLATER_ID = 167230743
+
+class DotaProfile:
+    def __init__(
+        self,
+        PROFILE_ID,
+        driver_options: Options = None
+    ) -> None:
+        self.PROFILE_ID = PROFILE_ID
+        self.driver = utils.set_up_driver(driver_options)
+        self._is_processed = False
+        
+    def get_last_match(self) -> DotaMatch:
+        self.driver.get(f"https://www.opendota.com/players/{self.PROFILE_ID}")
+        self._is_processed = True
+        
+        time.sleep(2)
+        with open("test.html", "w") as f:
+            f.write(self.driver.page_source)
+        body = self.driver.find_elements(By.TAG_NAME, "a")
+        
+        link_text = "https://www.opendota.com/matches/"
+        for el in body:
+            link = el.get_attribute("href")
+            if link_text in link:
+                match_id = int(link.removeprefix(link_text))
+                return DotaMatch(match_id)
+            
+        return None
+            
+        
+        
+if __name__ == "__main__":
+    profile = DotaProfile(167230743)
+    
+    profile.driver = utils.set_up_driver(debug=True)
+    profile.get_last_match()
