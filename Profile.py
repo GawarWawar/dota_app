ba1117714d61afc0c9d133dc665f447d4231f434
GawarWawar <gawarwawar@gmail.com>
2024-09-24 19:29:27 +0300

    added method to get all recent matches

--- Profile.py
+++ Profile.py
@@ -24,7 +24,7 @@ class DotaProfile:
             self.driver = utils.set_up_driver(driver_options)
         self._is_processed = False
         
-    def get_last_match(self) -> DotaMatch:
+    def get_last_match(self) -> DotaMatch|None:
         self.driver.get(f"https://www.opendota.com/players/{self.PROFILE_ID}")
         self._is_processed = True
         
@@ -41,6 +41,27 @@ class DotaProfile:
                 return DotaMatch(match_id, driver=self.driver)
             
         return None
+    
+    def get_resent_matches(self) -> list[DotaMatch]:
+        self.driver.get(f"https://www.opendota.com/players/{self.PROFILE_ID}")
+        self._is_processed = True
+        
+        time.sleep(2)
+        with open("test.html", "w") as f:
+            f.write(self.driver.page_source)
+        body = self.driver.find_elements(By.TAG_NAME, "a")
+        
+        link_text = "https://www.opendota.com/matches/"
+        matches = []
+        for el in body:
+            link = el.get_attribute("href")
+            if link_text in link:
+                match_id = int(link.removeprefix(link_text))
+                matches.append(
+                    DotaMatch(match_id, driver=self.driver)
+                )
+        return matches
+            
             
         
         
