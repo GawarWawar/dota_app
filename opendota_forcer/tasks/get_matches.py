9dea16f23fb62c076d3421251ebf1fe0e11e9730
GawarWawar <gawarwawar@gmail.com>
2024-09-26 15:41:22 +0300

    created task that will check parced status and then request a parce if needed
    also makes entries into database along the way

--- opendota_forcer/tasks/get_matches.py
+++ opendota_forcer/tasks/get_matches.py
@@ -1,3 +1,41 @@
 from celery import shared_task
 from celery_instance.celery_app import app
+import datetime
 
+
+from opendota_forcer import models
+
+from opendota_forcer.src.profile import DotaProfile
+from opendota_forcer.src.match import DotaMatch
+
+@shared_task(
+    bind = True,
+)
+def process_user_last_match(
+    self,
+    PROFILE_ID
+):
+    profile = DotaProfile(PROFILE_ID)
+    last_match = profile.get_last_match()
+    match_object = models.DotaMatch().objects.create(
+        id = last_match.MATCH_ID
+    )
+    match_object.save()
+    parced_before = last_match.check_parsed_status()
+    
+    self.message = last_match.parse_match()
+    
+    scan_object = models.Scan.objects.create(
+        profile_id = PROFILE_ID,
+        match_id = last_match.MATCH_ID,
+        parced_before = parced_before
+    )
+    scan_object.save()
+    
+    self.profile_id = PROFILE_ID
+    self.match_id = last_match.MATCH_ID
+    
+    return self.message 
+    
+        
+        
