efbbb360dd48b6503276ef903b0e16bff6bfb376
GawarWawar <gawarwawar@gmail.com>
2024-09-26 15:40:18 +0300

    added logger set up functions into utils

--- opendota_forcer/src/utils.py
+++ opendota_forcer/src/utils.py
@@ -1,3 +1,6 @@
+import logging    
+import os.path
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -16,3 +19,29 @@ def set_up_driver(
             driver_options.add_argument("--headless")
         driver_options.add_argument("--enable-javascript")
     return webdriver.Chrome(options=driver_options)
+
+
+# Create logger to make logs
+def get_logger (
+    logger_name: str,
+    log_level: str = "INFO",
+):     
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(log_level)
+    
+    return logger
+                
+def assign_filehandler_to_logger (
+    logger: logging.Logger,
+    file_location: tuple = ('data', 'actions.log')
+):       
+    file_handler = logging.FileHandler(
+        os.path.join(
+            file_location
+        )
+    )
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    return file_handler
