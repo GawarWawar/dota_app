ce34ac7985498b08308f613914d41818c3d27c6e
GawarWawar <gawarwawar@gmail.com>
2024-09-26 03:12:58 +0300

    added: - startup comands into celery_app as comments for Celery worker and Celery beat instance - settings needed for celery to work and discover tasks - created module to create tasks into

--- celery_instance/settings.py
+++ celery_instance/settings.py
@@ -1,7 +1,15 @@
 broker_url = 'redis://localhost:6379/0'
 broker_connection_retry_on_startup = True
+beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
+
 acks_late = True
 
 enable_utc = False
 task_track_started = True
 result_extended = True
+
+timezone = 'UTC'
+
+include=(
+    "opendota_forcer.tasks.get_matches"
+)
---
+++ opendota_forcer/tasks/get_matches.py
@@ -0,0 +1,3 @@
+from celery import shared_task
+from celery_instance.celery_app import app
+
