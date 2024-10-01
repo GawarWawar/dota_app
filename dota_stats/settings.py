10712599e150f95edbc1c2208b4a872e546f10d7
GawarWawar <gawarwawar@gmail.com>
2024-10-01 16:26:27 +0300

    added django_key as env variable

--- dota_stats/settings.py
+++ dota_stats/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^=w8@)#tqawinzafr@8-094_r^0k0&ag+o54r8a-kp@)rjeyzm'
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
