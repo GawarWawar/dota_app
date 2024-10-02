f3ad770c32918ebccedd332f28beddd4ff9fc0d9
GawarWawar <gawarwawar@gmail.com>
2024-10-03 01:03:38 +0300

    added return value and of the list with processed users and fixed column in model after renaming process

--- opendota_forcer/tasks/process_active_users.py
+++ opendota_forcer/tasks/process_active_users.py
@@ -1,20 +1,15 @@
 from celery import shared_task
-from celery_instance.celery_app import app
-import datetime
 
 from opendota_forcer import models
-
-from opendota_forcer.src.profile import DotaProfile
-from opendota_forcer.src.match import DotaMatch
-
 from .process_user_last_match import process_user_last_match
 
 
 @shared_task(
     bind = True,
 )
-def process_active_users(self):
-    active_users = models.DotaProfile.objects.filter(status = True)
+def process_active_users(self) -> list[tuple[int, str]]:
+
+    active_users = models.DotaProfile.objects.filter(is_active = True)
     
     users_processed = []
     for user in active_users:
@@ -22,3 +17,5 @@ def process_active_users(self):
         users_processed.append((user.id, user.name))
     
     self.users_processed = users_processed
+    
+    return users_processed
